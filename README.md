bb9103c91b3bb7b3723aa3d47778e479da79a851
Dev <dntiontk@gmail.com>
2023-07-16 00:37:57 -0400

    reorganize code to fit bootstrap theme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# windsor-opendata - precipitation
+# windsor-pricipitation-dashboard
 
-This directory contains a dashboard focused on visualizing precipitation data. The dashboard provides insights and analysis based on the available datasets.
+This project contains a dashboard focused on visualizing precipitation data in Windsor, Ontario. The dashboard provides insights and analysis based on the data collected from the [City of Windsor's OpenData Catalogue](https://opendata.citywindsor.ca/). The data has been pre-processed and made available here: https://github.com/dntiontk/windsor-opendata/tree/main/precipitation.
 
+## Contributing
+
+Contributions, issues, and pull requests are welcome! If you encounter any problems, have suggestions, or would like to contribute to the project, please submit an issue or pull request through GitHub.
--- app.py
+++ app.py
@@ -1,128 +1,246 @@
 #!/usr/bin/env python3
 
-from dash import Dash, html, dcc, Output, Input
+from dash import Dash, html, dcc, Output, Input, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from os import path
 import datetime
+import time
 
 # copy of cleaned data originally sourced from https://opendata.citywindsor.ca/
 DATA_SRC = "https://raw.githubusercontent.com/dntiontk/windsor-opendata/main/precipitation/windsor-precipitation.csv"
-TEMPLATE = "plotly_dark"
 
 df = pd.read_csv(DATA_SRC)
-
 df["DateTime"] = pd.DatetimeIndex(df["DateTime"])
-
-first, last = df["DateTime"].dt.year.min(), df["DateTime"].dt.year.max()
-unique_years = df["DateTime"].dt.year.unique()
-gauges = df["Gauge"].unique()
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 server = app.server
 
-app.layout = html.Div(
+jumbotron = html.Div(
+    id="jumbotron",
+    children=[
+        dbc.Container(
+            [
+                html.H1(
+                    "Windsor Precipitation Dashboard", className="display-3 text-center"
+                ),
+                html.P(
+                    [
+                        """ This project contains a dashboard focused on visualizing precipitation data in Windsor, Ontario. The dashboard provides insights and analysis based on the data collected from the City of Windsor's OpenData Catalogue. It is licensed by the Open Government License – The Corporation of the City of Windsor.
+                        For more information, please see us """,
+                        html.A(
+                            "here.",
+                            href="https://github.com/dntiontk/precipitation-dashboard",
+                        ),
+                    ]
+                ),
+                html.Hr(),
+            ],
+            fluid=True,
+            className="py-3",
+        ),
+    ],
+    className="bg-dark",
+)
+
+
+def select_all_button(id):
+    return dbc.Button(
+        id=id,
+        size="sm",
+        style={"margin-bottom": "10px"},
+        children=["Select/Deselect All"],
+    )
+
+
+gauge_list = df["Gauge"].unique()
+gauge_select = select_all_button("gauge-select")
+gauge_checklist = dcc.Checklist(
+    id="gauge-checklist",
+    options=gauge_list,
+    value=[],
+    inputStyle={"margin-right": "20px"},
+)
+
+year_list = df["DateTime"].dt.year.unique()
+first = year_list[0]
+last = year_list[len(year_list) - 1]
+year_slider = dcc.RangeSlider(
+    id="year-slider",
+    min=first,
+    max=last,
+    value=[first, last],
+    allowCross=False,
+    step=1,
+    marks={str(i): str(i) for i in range(first, last + 1)},
+)
+
+filter_cards = dbc.CardGroup(
+    id="filter-cards",
+    children=[
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5("Select gauges", className="card-title"),
+                    gauge_select,
+                    gauge_checklist,
+                ]
+            )
+        ),
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5(
+                        "Select years",
+                        className="card-title",
+                        style={"margin-bottom": "20px"},
+                    ),
+                    year_slider,
+                ]
+            )
+        ),
+        dbc.Card(),
+        dbc.Card(),
+    ],
+)
+
+filter_accordion = html.Div(
+    id="filter-accordion",
+    children=[
+        dbc.Accordion(
+            [
+                dbc.AccordionItem(
+                    [filter_cards],
+                    title="Filters",
+                )
+            ],
+            start_collapsed=True,
+        )
+    ],
+)
+
+total_rainfall_card = dbc.Card(
+    children=[
+        dbc.CardBody(
+            children=[
+                dcc.Graph(id="total-rainfall-graph"),
+            ],
+        )
+    ],
+)
+
+gauge_distribution_card = dbc.Card(
+    children=[dbc.CardBody(children=[dcc.Graph(id="gauge-distribution-graph")])],
+)
+
+box_plot_accumulation_card = dbc.Card(
+    children=[dbc.CardBody(children=[dcc.Graph(id="box-plot-accumulation")])],
+)
+
+top_row_cards = dbc.Row(
     [
-        html.H1(children="Windsor Precipitation Dashboard ({}-{})".format(first, last)),
-        html.P(
-            "This dashboard provides precipitation data for the City of Windsor, Ontario. The data used for these contains information licensed under the Open Government License - The Corporation of the City of Windsor"
-        ),
-        html.Div("Gauge contribution to annual rainfall distribution"),
-        dcc.Graph(id="gauge-contribution-pie"),
-        # dcc.Dropdown(
-        #    id="year-selector", options=unique_years, value=last, clearable=False, tem
-        # ),
-        dcc.RangeSlider(
-            first,
-            last,
-            value=[last - 1, last],
-            id="year-slider",
-            allowCross=False,
-            step=1,
-            marks={i: "{}".format(i) for i in range(first, last + 1)},
-        ),
-        html.Div("Annual cumulative rainfall composition by gauge over time"),
-        dcc.Graph(id="cumulative-rainfall-stacked"),
-        dcc.Dropdown(
-            id="collector-style",
-            options=["Rainfall Total", "Daily Accumulation"],
-            value="Rainfall Total",
-        ),
+        dbc.Col(gauge_distribution_card, width=5),
+        dbc.Col(box_plot_accumulation_card, width=7),
+    ]
+)
+
+app.layout = html.Div(
+    children=[
+        jumbotron,
+        filter_accordion,
+        html.Div(children=[top_row_cards]),
+        total_rainfall_card,
     ],
 )
 
 
 @app.callback(
-    Output("gauge-contribution-pie", "figure"),
-    Input("year-slider", "value"),
+    Output("total-rainfall-graph", "figure"),
+    [Input("gauge-checklist", "value"), Input("year-slider", "value")],
 )
-def update_pie_graph(value):
-    start = value[0]
-    end = value[len(value) - 1]
+def total_rainfall_graph_update(gauges, years):
+    first = years[0]
+    last = years[len(years) - 1]
     filtered_df = df[
-        (df["DateTime"].dt.year >= start) & (df["DateTime"].dt.year <= end)
+        (df["Gauge"].isin(gauges))
+        & (df["DateTime"].dt.year >= first)
+        & (df["DateTime"].dt.year <= last)
     ]
-
-    annual_rainfall = filtered_df.groupby("Gauge")["Rainfall Total"].sum()
-    gauge_contribution = (annual_rainfall / annual_rainfall.sum()) * 100
-    gauge_frame = gauge_contribution.to_frame().reset_index()
-
-    fig = px.pie(
-        gauge_frame,
-        names="Gauge",
-        values="Rainfall Total",
-        title="Annual rainfall distribution ({}-{})".format(start, end),
-        template=TEMPLATE,
+    fig = px.line(
+        filtered_df,
+        x="DateTime",
+        y="Rainfall Total",
+        color="Gauge",
+        color_discrete_sequence=px.colors.qualitative.Dark24,
+        template="plotly_dark",
     )
     return fig
 
 
 @app.callback(
-    Output("cumulative-rainfall-stacked", "figure"),
-    Input("year-slider", "value"),
+    Output("box-plot-accumulation", "figure"),
+    [Input("gauge-checklist", "value"), Input("year-slider", "value")],
 )
-def update_stacked_chart(value):
-    start = value[0]
-    end = value[len(value) - 1]
+def box_accumulation_update(gauges, years):
+    first = years[0]
+    last = years[len(years) - 1]
     filtered_df = df[
-        (df["DateTime"].dt.year >= start) & (df["DateTime"].dt.year <= end)
+        (df["Gauge"].isin(gauges))
+        & (df["DateTime"].dt.year >= first)
+        & (df["DateTime"].dt.year <= last)
     ]
-
-    fig = px.area(
+    fig = px.histogram(
         filtered_df,
         x="DateTime",
-        y="Rainfall Total",
+        y="Daily Accumulation",
         color="Gauge",
+        color_discrete_sequence=px.colors.qualitative.Dark24,
+        template="plotly_dark",
     )
     return fig
-    # [
-    #    html.P("Precipitation per gauge"),
-    #    dcc.Graph(
-    #        id="total",
-    #        figure=px.line(
-    #            df,
-    #            x="DateTime",
-    #            y="Rainfall Total",
-    #            labels={"Rainfall Total": "mm/hr"},
-    #            color="Gauge",
-    #            title="Rainfall Total",
-    #        ),
-    #    ),
-    #    html.Hr(),
-    #    dcc.Graph(
-    #        id="daily",
-    #        figure=px.line(
-    #            df,
-    #            x="DateTime",
-    #            y="Daily Accumulation",
-    #            labels={"Daily Accumulation": "mm/hr"},
-    #            color="Gauge",
-    #            title="Daily Accumulation",
-    #        ),
-    #    ),
-    # ]
+
+
+@app.callback(
+    Output("gauge-distribution-graph", "figure"),
+    [Input("gauge-checklist", "value"), Input("year-slider", "value")],
+)
+def gauge_distribution_graph_update(gauges, years):
+    first = years[0]
+    last = years[len(years) - 1]
+    filtered_df = df[
+        (df["Gauge"].isin(gauges))
+        & (df["DateTime"].dt.year >= first)
+        & (df["DateTime"].dt.year <= last)
+    ]
+    rainfall = filtered_df.groupby("Gauge")["Rainfall Total"].sum()
+    contribution = (rainfall / rainfall.sum()) * 100
+    gauge_frame = contribution.to_frame().reset_index()
+    fig = px.pie(
+        gauge_frame,
+        names="Gauge",
+        values="Rainfall Total",
+        title="Rainfall distribution ({}-{})".format(first, last),
+        color_discrete_sequence=px.colors.qualitative.Dark24,
+        template="plotly_dark",
+    )
+    return fig
+
+
+@app.callback(
+    Output("gauge-checklist", "value"),
+    [Input("gauge-select", "n_clicks")],
+    [State("gauge-checklist", "value")],
+)
+def on_gauge_click(n, value):
+    if n is None:
+        return gauge_list
+    else:
+        if value:
+            return []
+        else:
+            return gauge_list
 
 
 if __name__ == "__main__":
